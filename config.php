86bf29e3e5371de202c53356d037069f7476d627
Andrés Guerrero <andresguerrero016@gmail.com>
2023-10-16 13:55:45 -0300

    Cambios

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 
 const MYSQL_USER = 'root';
 const MYSQL_PASS = '';
-const MYSQL_DB = 'db_tienda_de_calzado';
+const MYSQL_DB = 'db_shoespot';
 const MYSQL_HOST = 'localhost';
 
--- database/db_shoespot.sql
+++ database/db_shoespot.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-10-2023 a las 01:24:30
+-- Tiempo de generación: 16-10-2023 a las 18:29:31
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `db_tienda_de_calzado`
+-- Base de datos: `db_shoespot`
 --
 
 -- --------------------------------------------------------
